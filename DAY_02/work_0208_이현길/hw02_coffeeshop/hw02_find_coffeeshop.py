7d58587315ed9dd4ed8dfe5ec5bba7204ef88360
Schubert3275 <dragon3275@gmail.com>
2024-02-12 13:50:39 +0900

    크롤링 2일차

--- DAY_02/work_0208_이현길/hw02_coffeeshop/hw02_find_coffeeshop.py
+++ DAY_02/work_0208_이현길/hw02_coffeeshop/hw02_find_coffeeshop.py
@@ -2,19 +2,17 @@ import pandas as pd
 
 file = "hollys_branches.csv"
 storeDF = pd.read_csv(file)
-# print(storeDF)
 
 while True:
     print('-' * 20)
     city = input("검색할 매장의 도시를 입력하세요(quit 입력으로 종료): ")
-    # city = "서울 종로"
     if city == 'quit':
         print("종료합니다.")
         break
-    print('-' * 20)
     serchDF = storeDF[storeDF['위치(시,구)'].str.contains(city)]
     serchDF = serchDF[['주소', '전화번호']]
     num = serchDF.shape[0]
+    print('-' * 20)
     print(f'검색된 매장 수:  {num}')
     print('-' * 20)
     for i in range(num):
